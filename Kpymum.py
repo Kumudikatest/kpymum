e29c8264ddb9d150f1093475f3a746a22c1b8265
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2022-04-25 11:42:19 +0530

    cog res

--- Kpymum.py
+++ Kpymum.py
@@ -1,23 +1,13 @@
 import boto3
+cognito_idp = boto3.client("cognito-idp")
+from os import environ
 ses = boto3.client("ses")
 
 def handler(event, context):
     try:
-        data = ses.send_email(
-            Source="testsigma18+April20th2022GSK@gmail.com",
-            Destination={
-                'ToAddresses': ["kumudika@adroitlogic.com"]
-            },
-            Message={
-                'Subject': {
-                    'Data': "tt"
-                },
-                'Body': {
-                    'Html': {
-                        'Data': ""
-                    }
-                }
-            }
+        data = cognito_idp.list_users(
+            UserPoolId=environ["UserPoolId_cognitoKcogpy"],
+            Limit=10
         )
         print(data)
     except BaseException as e:
